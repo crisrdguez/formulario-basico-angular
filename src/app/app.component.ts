04d37ea11acdab5755b5f37b65dd4f6cc5c2cd71
crisRR <140411058+crisrdguez@users.noreply.github.com>
2023-08-01 13:34:20 +0200

    formulario uso recopilatorio ngfor, ngClass, Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'empleadosApp';
+
+  title = 'Listado de Empleados';
+  nombre:string="";
+  apellidos:string="";
+  cargo:string="";
+  salario:number=0;
+
+  empleados:Empleado[]=[];
+
+  constructor() {
+    const empleado1 = new Empleado("Cris", "RR", "jefa", 5000);
+    const empleado2 = new Empleado("Fran", "FG", "secretario", 3000);
+    const empleado3 = new Empleado("Juan", "MN", "administrativo", 2000);
+
+    this.empleados.push(empleado1, empleado2, empleado3);
+  }
+
+  enviarInformacion(){
+    const empleadoNuevo = new Empleado(this.nombre,this.apellidos,this.cargo,this.salario);
+
+    this.empleados.push(empleadoNuevo);
+
+    this.resetInfo();
+  }
+
+  resetInfo(){
+    this.nombre="";
+    this.apellidos="";
+    this.cargo="";
+    this.salario=0;
+  }
+  
+
 }
